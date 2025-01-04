5286575c9ed7dd7c1dc9cbbd65302950dc60c53c
Taiwo Ifeoluwase <105116838+TeeHigh@users.noreply.github.com>
2025-01-04 01:35:53 +0100

    Minor fixes

--- src/components/FirstPage/hero.jsx
+++ src/components/FirstPage/hero.jsx
@@ -11,13 +11,13 @@ const Hero = () => {
   });
   return (
     <div className="max-h-full">
-      <div className=" h-[100vh] max-w-full hero">
+      <div className=" h-[90vh] max-w-full hero">
         <div className="w-full absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] flex flex-col  justify-center items-center">
           <h1 className="text-center font-bold rounded-[0.625rem] px-[1.1325rem] py-[0.8125rem] my-6 w-fit max-w-[90%] nacostext bg-[#FAB5B5] text-[--white] font-Poppins leading-[157.5%] text-[1.3rem] text-[#F8CDCD]">
             Welcome To NACOS BOWEN CHAPTER
           </h1>
 
-          <p className="text-white md:text-[3rem] sm:w-[88%] text-center sm:text-[2rem] text-[1.4rem] font-semibold font-Poppins lg:text-[3.25981rem]">
+          <p className="text-white sm:w-[88%] text-center font-semibold font-Poppins text-[1.4rem] sm:text-[2rem] md:text-[3rem] lg:text-[3.25981rem] my-6">
             Empowering Future Innovators through
             <br />
             {text}
@@ -27,7 +27,7 @@ const Hero = () => {
             </span>
           </p>
 
-          <p className="text-center text-white lg:text-[1.451rem] md:text-[1rem] text-[0.8rem] py-3 leading-[157.5%] font-normal sm:w-[60%] ">
+          <p className="text-center text-white lg:text-[1.451rem] md:text-[1rem] text-[0.95rem] py-3 mt-10 leading-[157.5%] font-normal sm:w-[60%] max-w-[95%]">
             NACOS is a computing department in Bowen University that leverages on
             effective learning through advance technology and tools.
           </p>
